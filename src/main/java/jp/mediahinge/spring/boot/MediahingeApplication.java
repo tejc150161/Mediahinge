c7994e57c12bf6a48aa4ca91a6366c7ecf2f3cee
150161 <150161@e0601-01.JC-21.local>
2018-11-19 16:06:21 +0900

    create Mediahinge

---
+++ src/main/java/jp/mediahinge/spring/boot/MediahingeApplication.java
@@ -0,0 +1,12 @@
+package jp.mediahinge.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediahingeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediahingeApplication.class, args);
+	}
+}
