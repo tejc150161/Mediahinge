bc69f46264da6a65f748416f1b404352538a54fc
150527 <150527@e0601-02.JC-21.local>
2018-11-29 14:43:25 +0900

    rss取得して出力できた

--- src/main/java/jp/mediahinge/spring/boot/GetRss.java
+++ src/main/java/jp/mediahinge/spring/boot/GetRss.java
@@ -1,7 +1,17 @@
 package jp.mediahinge.spring.boot;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.SocketAddress;
 import java.net.URL;
+import java.net.URLConnection;
+
 import com.sun.syndication.feed.synd.SyndEntry;
 import com.sun.syndication.feed.synd.SyndFeed;
 import com.sun.syndication.io.SyndFeedInput;
@@ -9,27 +19,63 @@ import com.sun.syndication.io.XmlReader;
 
 public class GetRss {
     public static void main(String[] args) throws Exception {
-        String url = "http://gihyo.jp/feed/rss1";
-        // String url = "http://rss.rssad.jp/rss/gihyo/feed/rss2";
-        // String url = "http://rss.rssad.jp/rss/gihyo/feed/atom";
+    	
+    	String urlstr = "http://www3.asahi.com/rss/politics.rdf";
 
-        URL feedUrl = new URL(url);
         SyndFeedInput input = new SyndFeedInput();
-        SyndFeed feed = input.build(new XmlReader(feedUrl));
+        URL url = new URL(urlstr);
+        URLConnection urlConnection;
         
-        // サイトのタイトル
-        System.out.println(feed.getTitle());
-        // サイトのURL
-        System.out.println(feed.getLink());
+        SocketAddress addr = new InetSocketAddress("172.17.0.2", 80);
+        Proxy proxy = new Proxy(Proxy.Type.HTTP,addr);
+        urlConnection = url.openConnection(proxy);
 
+        InputStream str = urlConnection.getInputStream();
+        SyndFeed feed = input.build(new XmlReader(urlConnection));
+
+        
+        // 出力ファイルの作成
+        FileWriter f = new FileWriter("E:\\git\\sample.csv", false);
+        PrintWriter p = new PrintWriter(new BufferedWriter(f));
+        
+        // 記事リンク取得
+        p.print(feed.getTitle());
+        p.print(",");
         for (Object obj : feed.getEntries()) {
-            SyndEntry entry = (SyndEntry) obj;
-            // 記事タイトル
-            System.out.println(entry.getTitle());
-            // 記事のURL
-            System.out.println(entry.getLink());
-            // 記事の詳細
-            System.out.println(entry.getDescription().getValue());
+        	SyndEntry entry = (SyndEntry) obj;
+            p.print(entry.getLink());
+            p.print(",");
         }
+
+        // ファイルに書き出し閉じる
+        p.close();
+        System.out.println("ファイル出力完了！");
+        
+        
+        
+//		urlConnection=url.openConnection();
+//		  URL feedUrl = new URL(url);
+//      HttpURLConnection urlConnection = null;
+//      try {
+//       Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("172.17.0.2", 80));
+//       urlConnection = (HttpURLConnection) url.openConnection(proxy);
+//       urlConnection.setRequestMethod("GET");
+//       urlConnection.connect();
+//      } catch (Exception e) {
+//       e.printStackTrace();
+//      }
+//        // サイトのURL
+//        System.out.println(feed.getLink());
+
+//        for (Object obj : feed.getEntries()) {
+//            SyndEntry entry = (SyndEntry) obj;
+//            // 記事タイトル
+//            System.out.println(entry.getTitle());
+//            // 記事のURL
+//            System.out.println(entry.getLink());
+//            // 記事の詳細
+//            System.out.println(entry.getDescription().getValue());
+//        }
+        
     }
 }
